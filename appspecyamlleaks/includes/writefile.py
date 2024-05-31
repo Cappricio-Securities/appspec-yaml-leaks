99e1f538231de32ab0bbeefc3f1078d520c9635d
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2024-05-31 20:10:35 +0530

    Tool to find sensitive leaks (Appspec Yaml) 🥷🏾

---
+++ appspecyamlleaks/includes/writefile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+ * appspec-yaml-leaks
+ * appspec-yaml-leaks Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+def writedata(output, data):
+    with open(output, 'a') as file:
+        file.write(data)
